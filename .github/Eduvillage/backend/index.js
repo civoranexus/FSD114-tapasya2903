dfaa0b1d8df8ada032fea176f3325b8a907818fc
tapasya2903 <tapasyak77@gmail.com>
2026-01-16 12:24:01 +0530

    feat: initialize express backend server

---
+++ .github/Eduvillage/backend/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+const PORT = 5000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("EduVillage backend running");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
