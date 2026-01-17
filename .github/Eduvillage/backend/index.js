d185a94d09d57b8e7afd71bed6909886454eb835
tapasya2903 <tapasyak77@gmail.com>
2026-01-17 10:25:52 +0530

    refactor: move api route to separate routes folder

--- .github/Eduvillage/backend/index.js
+++ .github/Eduvillage/backend/index.js
@@ -3,12 +3,19 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 
+// middleware
 app.use(express.json());
 
+// routes
+const healthRoutes = require("./routes/health.route");
+app.use("/api", healthRoutes);
+
+// base route
 app.get("/", (req, res) => {
   res.send("EduVillage backend running");
 });
 
+// start server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
