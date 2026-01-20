9a23329ff2e06181e744a0abc66f601e5c80161e
tapasya2903 <tapasyak77@gmail.com>
2026-01-20 11:55:09 +0530

    setup mongodb connection and user api

--- .github/Eduvillage/backend/index.js
+++ .github/Eduvillage/backend/index.js
@@ -1,14 +1,22 @@
+require("dotenv").config();
+const connectDB = require("./config/db");
 const express = require("express");
 
 const app = express();
 const PORT = 5000;
+
+// connect database
+connectDB();
 
 // middleware
 app.use(express.json());
 
 // routes
 const healthRoutes = require("./routes/health.route");
+const userRoutes = require("./routes/user.route");
 app.use("/api", healthRoutes);
+app.use("/api/users", userRoutes);
+
 
 // base route
 app.get("/", (req, res) => {
